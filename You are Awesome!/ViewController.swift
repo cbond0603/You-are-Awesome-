d09b21774e4050de324ad1a3cd27053efbe0c154
Chris Bond <bondcb@bc.edu>
2022-01-26 19:04:21 -0500

    Let, If, Else Statements

--- You are Awesome!/ViewController.swift
+++ You are Awesome!/ViewController.swift
@@ -10,18 +10,32 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
-    
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        messageLabel.text = ""
+        
         
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        messageLabel.text = "You Are Awesome!"
-        imageView.image = UIImage(named: "image0")
+        let awesomeMessage = "You Are Awesome!"
+        let bombMessage = "You Are Da Bomb!"
+        let greatMessage = "You Are Great!"
+        
+        
+        
+        if messageLabel.text == awesomeMessage {
+            messageLabel.text = greatMessage
+            imageView.image = UIImage(named: "image0")
+        } else if  messageLabel.text == greatMessage{
+            messageLabel.text = bombMessage
+            imageView.image = UIImage(named: "image1")
+        } else {
+            messageLabel.text = awesomeMessage
+            imageView.image = UIImage(named: "image2")
+            
+        }
     }
     
 }
