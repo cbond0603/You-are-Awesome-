53e464b12bde4b747a309732f46d3f189485873d
Chris Bond <bondcb@bc.edu>
2022-01-27 11:43:32 -0500

    Ch 2.11: Randomization

--- You are Awesome!/ViewController.swift
+++ You are Awesome!/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     var imageNumber = 0
     var messageNumber = 0
+    let totalNumberOfImages = 9
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +23,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let messages = ["You Are Awesome!", "You Are Da Bomb!", "You Are Great!", "Fabulous? That's You"]
+        let messages = ["You Are Awesome!", "You Are Da Bomb!", "You Are Great!", "Fabulous? That's You","When the Genius Bar Needs Help they Call You", "You've got the design skills of Johnny Ive"]
+        messageLabel.text = messages[Int.random(in: 0...messages.count-1)]
+        imageView.image = UIImage(named: "image\(Int.random(in:0...totalNumberOfImages))")
         
-        messageLabel.text = messages[messageNumber]
-        messageNumber += 1
-        if messageNumber == messages.count{
-            messageNumber = 0
-        }
+        
+//        messageLabel.text = messages[messageNumber]
+//        messageNumber += 1
+//        if messageNumber == messages.count{
+//            messageNumber = 0
+//        }
 
         print(imageNumber)
         //let imageName = "image" + String(imageNumber)
